8bdddd07478c51f4f2a0d7acc3ed9adfebb336df
Pipo Bizelli <pipo.bizelli@gmail.com>
2019-08-21 17:08:21 -0300

    add store (vuex) to board editor

--- pages/canvas/index.vue
+++ pages/canvas/index.vue
@@ -1,6 +1,5 @@
 <template>
   <section>
-    <h1>Canvas</h1>
   </section>
 </template>
 
--- store/board.js
+++ store/board.js
@@ -9,38 +9,11 @@ export const state = () => ({
     config: BoardConfig.config
   },
   selectedTiles: [],
-  tiles: {}
+  disabledTiles: []
 })
 
 export const mutations = {
-  set_tiles (state, config) {
-    state.tiles = config
-  },
   set_selected (state, tilesArr) {
     state.selectedTiles = tilesArr
-  }
-}
-
-export const actions = {
-  init ({ state, commit, dispatch }) {
-    let config = {}
-    for (let l = 0; l < BoardConfig.lines; l++) {
-      for (let c = 0; c < BoardConfig.columns; c++) {
-        config = {
-          ...config,
-          [`${l}:${c}`]: {
-            ...BoardConfig.config[`${l}:${c}`],
-            handle: `${l}:${c}`,
-            fill: 'transparent',
-            opacity: 0.5,
-            x: (BoardConfig.tile.width * c),
-            y: (BoardConfig.tile.height * l),
-            width: BoardConfig.tile.width,
-            height: BoardConfig.tile.height
-          }
-        }
-      }
-    }
-    commit('set_tiles', config)
   }
 }
